756afe4d880e9a65b1467bb4ed774970b0d21b9e
m2costa <matheus_mc@live.com>
2018-10-12 21:40:23 -0300

    Alterando header, footer e menu lateral, adicionando (em progresso) e adicionando fontes e diagrama de cores do projeto

---
+++ components/footer.js
@@ -0,0 +1,85 @@
+import '../scss/style.scss';
+
+import React, { Component } from 'react';
+
+class Footer extends Component {
+	constructor(props) {
+		super(props);
+	}
+    
+	render(){
+		return(
+			<div className="footerContainer col-12">
+
+				<div className="footerDiv col-2">
+					<a>
+                        Labore adipisicing quis culpa esse exercitation.
+					</a>
+					<a>
+                        Labore adipisicing quis culpa esse exercitation.
+					</a>
+					<a>
+                        Labore adipisicing quis culpa esse exercitation.
+					</a>
+				</div>
+				<div className="footerDiv col-2">
+					<a>
+                        Labore adipisicing quis culpa esse exercitation.
+					</a>
+					<a>
+                        Labore adipisicing quis culpa esse exercitation.
+					</a>
+					<a>
+                        Labore adipisicing quis culpa esse exercitation.
+					</a>
+				</div>
+				<div className="footerDiv col-2">
+					<a>
+                        Labore adipisicing quis culpa esse exercitation.
+					</a>
+					<a>
+                        Labore adipisicing quis culpa esse exercitation.
+					</a>
+					<a>
+                        Labore adipisicing quis culpa esse exercitation.
+					</a>
+				</div>
+				<div className="footerDiv col-2">
+					<a>
+                        Labore adipisicing quis culpa esse exercitation.
+					</a>
+					<a>
+                        Labore adipisicing quis culpa esse exercitation.
+					</a>
+					<a>
+                        Labore adipisicing quis culpa esse exercitation.
+					</a>
+				</div>
+				<div className="footerDiv col-2">
+					<a>
+                        Labore adipisicing quis culpa esse exercitation.
+					</a>
+					<a>
+                        Labore adipisicing quis culpa esse exercitation.
+					</a>
+					<a>
+                        Labore adipisicing quis culpa esse exercitation.
+					</a>
+				</div>
+				<div className="footerDiv col-2">
+					<a>
+                        Labore adipisicing quis culpa esse exercitation.
+					</a>
+					<a>
+                        Labore adipisicing quis culpa esse exercitation.
+					</a>
+					<a>
+                        Labore adipisicing quis culpa esse exercitation.
+					</a>
+				</div>    
+			</div>
+		);
+	}
+}
+
+export default Footer;
